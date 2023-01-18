78bbd6da8448ef0b6292215c1add05d41ae554a5
zsorosz <oroszi.zsofi@gmail.com>
2023-01-18 18:26:30 +0100

    Iteration-5 done

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function updateSubtotal(product) {
   const price = product.querySelector(".price span").innerText;
   const quantity = product.querySelector(".quantity input").value;
 
-  const subtotal = parseInt(price) * parseInt(quantity);
+  const subtotal = parseFloat(price) * parseFloat(quantity);
   const subtotalEl = product.querySelector(".subtotal");
   subtotalEl.innerText = subtotal;
 
@@ -33,15 +33,30 @@ function calculateAll() {
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log("The target in remove is:", target);
   target.parentNode.parentNode.remove();
+  console.log(target.parentNode.parentNode);
   calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  const cart = document.querySelector("#cart tbody");
+  const product = document.querySelector("#cart tbody tr");
+  const newProduct = product.cloneNode(true);
+
+  const name = newProduct.querySelector(".name span");
+  const newName = document.querySelector(".create-product td input");
+  name.innerText = newName.value;
+
+  const price = newProduct.querySelector(".price span");
+  const newPrice = document.querySelectorAll(".create-product td input")[1];
+  price.innerText = newPrice.value;
+
+  cart.appendChild(newProduct);
+
+  const removeBtn = newProduct.querySelector(".btn-remove");
+  removeBtn.addEventListener("click", removeProduct);
 }
 
 window.addEventListener("load", () => {
@@ -51,5 +66,15 @@ window.addEventListener("load", () => {
   const removeBtns = document.querySelectorAll(".btn-remove");
   removeBtns.forEach((btn) => {
     btn.addEventListener("click", removeProduct);
+  });
+
+  const createBtn = document.getElementById("create");
+  createBtn.addEventListener("click", () => {
+    createProduct();
+    const inputName = document.querySelector(".create-product td input");
+    inputName.value = "";
+    const inputPrice = document.querySelectorAll(".create-product td input")[1];
+    inputPrice.value = 0;
+    inputName.focus();
   });
 });
